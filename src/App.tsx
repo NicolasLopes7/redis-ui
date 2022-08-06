c180dde1c256f0826b9db839bf72d902a44ca19b
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-08-06 11:45:32 -0300

    style(lint)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import './App.css';
 import { invoke } from '@tauri-apps/api';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
 
@@ -20,12 +19,7 @@ function App() {
       <div></div>
       <h1>Vite + React</h1>
       <div className="card">
-        <input
-          type="text"
-          placeholder="Your name"
-          value={name}
-          onChange={handleChangeName}
-        ></input>
+        <input type="text" placeholder="Your name" value={name} onChange={handleChangeName}></input>
         <button type="submit" onClick={handleSubmit}>
           Submit
         </button>
