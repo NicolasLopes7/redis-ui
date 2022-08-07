e6241e59aedddc64f03da03a861d1589e9bc432d
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-08-07 19:34:12 -0300

    chore(seed-redis): creating script to seed redis

--- electron/services/redis/seed.ts
+++ electron/services/redis/seed.ts
@@ -1,13 +1,40 @@
 import Redis from 'ioredis';
+import Chance from 'chance';
+
+const productRandomData = new Chance();
 
 const DEFAULT_REDIS_URL = 'redis://localhost:6379';
 
-const products = [];
+const products = Array(100)
+  .fill({})
+  .map(() => ({
+    id: productRandomData.guid(),
+    name: productRandomData.name(),
+    price: productRandomData.floating({ min: 0, max: 100 }),
+    description: productRandomData.paragraph()
+  }));
 
 const seed = async () => {
   const client = new Redis(DEFAULT_REDIS_URL);
-};
 
+  const productsGroupedById = products.reduce((acc, curr) => ({
+    ...acc,
+    [curr.id]: curr
+  }));
+  await client.set('products', 'aaa');
+  return client
+    .pipeline(
+      Object.entries(productsGroupedById).map(([key, value]) => ['set', 'produtcs', key, JSON.stringify(value)])
+    )
+    .exec();
+};
 (async () => {
-  await seed();
+  console.log('🚀 Starting seeding Redis');
+  try {
+    seed();
+    console.log('✅ Finished seeding Redis');
+    return;
+  } catch (error) {
+    console.log('❌ Finished seeding Redis');
+  }
 })();
