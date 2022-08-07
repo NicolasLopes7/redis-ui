b1e28cfa3b2596a82e0cc27becf8adde6d40d8f7
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-08-07 20:03:20 -0300

    wip

--- src/pages/FirstConnection/index.tsx
+++ src/pages/FirstConnection/index.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { FormEvent } from 'react';
 import { Box, OutlineButton } from '../../components/base';
 import { Card } from '../../components/base/Card';
 import { TextInput } from '../../components/base/TextInput';
+import { useToastProvider } from '../../contexts/ToastProvider';
 
 export function FirstConnectionPage() {
+  const { addToast } = useToastProvider();
+
+  const handleConnect = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('calling');
+    addToast({ title: 'Could not connect', message: 'Please check your connection URL', type: 'error' });
+  };
+
   return (
     <Card css={{ gap: '24px', alignItems: 'center', width: '600px' }}>
       <Box
+        as="form"
+        onSubmit={handleConnect}
         css={{
           display: 'flex',
           alignItems: 'center',
@@ -21,6 +32,7 @@ export function FirstConnectionPage() {
           placeholder="Type your connection url"
         />
         <OutlineButton
+          type="submit"
           css={{
             fontSize: '$lg'
           }}
