5c9598dd5c9970f4625a821ecfbba551c94d2755
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-11-21 17:35:02 +0000

    feat: add toasts on setup connection

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -2,13 +2,21 @@ import React, { useCallback, useState } from 'react';
 
 import { ConnectionSelector, SetupConnection, useSavedConnections, Connection } from '@redis-ui/connections';
 
-import { Background, Flex } from '@redis-ui/ui';
+import { Background, Flex, useToastProvider } from '@redis-ui/ui';
 
 export function InitialPage() {
   const [selectedConnection, setSelectedConnection] = useState<Connection>();
   const { connections, saveConnection, removeConnection } = useSavedConnections();
+  const { activeToasts, addToast } = useToastProvider();
 
+  console.log({ activeToasts });
   const handleConnect = useCallback(async (connection: Connection) => {
+    const { remove: removeLoadingToast } = addToast({
+      title: 'Connecting...',
+      message: 'We are getting into you Redis instance...',
+      type: 'info'
+    });
+
     try {
       const { host, password, port, database } = connection.data;
 
@@ -16,6 +24,13 @@ export function InitialPage() {
 
       const response = await window.Main.connect(connectionURL);
       if (!response) {
+        removeLoadingToast();
+
+        addToast({
+          title: 'Unable to Connect',
+          message: "Oops! We weren't able to connect into your Redis instance",
+          type: 'error'
+        });
         return;
       }
 
@@ -23,8 +38,16 @@ export function InitialPage() {
         saveConnection(connection);
       }
 
-      console.log('Able to connect!');
+      removeLoadingToast();
+      addToast({ title: 'Connected!', message: 'We connected into your Redis instance', type: 'success' });
     } catch (error) {
+      removeLoadingToast();
+
+      addToast({
+        title: 'Unable to Connect',
+        message: "Oops! We weren't able to connect into your Redis",
+        type: 'error'
+      });
       console.error(error);
     }
   }, []);
